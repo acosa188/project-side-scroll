9835c6618d13e78a5acf21333b2a5aabf26de3f3
Arjun Cosare <arjun.cosare@outlook.com>
2020-04-20 12:58:13 -0600

    Support large screen fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Intro from './components/intro'
 import Navigation from './components/navigation'
 import FooterLinks from './components/footerLinks'
 import ProjectList from './components/projectList'
-import TalksList from './components/talksList'
+import ReadsList from './components/readsList'
 
 function App() {
   return (
       <div className="App">
           <div className="row no-gutters ac-main-wrapper">
-              <div className="col-4 ">
+              <div className="col-5 col-xl-4 ">
                   <div className="ac-intro-wrapper">
                       <Intro />
                       <Navigation />
@@ -17,10 +17,10 @@ function App() {
                   </div>
                   
               </div>
-              <div className="col-8 ">
-                  <div className="ac-content-wrapper" data-spy="scroll" data-target="#ac-navigation" data-offset="0">
+              <div className="col-7 col-xl-8 ">
+                  <div className="ac-content-wrapper p-5 px-lg-3" data-spy="scroll" data-target="#ac-navigation" data-offset="0">
                       <ProjectList id="projectID" />
-                      <TalksList id="coursesID" />
+                      <ReadsList id="coursesID" />
                   </div>
               </div>
           </div>
--- src/components/footerLinks.js
+++ src/components/footerLinks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import '../css/footerLinks.css'
 
 class FooterLinks extends Component {
     render() {
@@ -9,12 +10,12 @@ class FooterLinks extends Component {
             <div className="footerLinksClass d-flex justify-content-start text-light py-lg-3 p-5">
                 <a className="ac-link mr-5" href="https://www.linkedin.com/in/arjun-cosare-350601154/">        
                     <span className="footer-font"> Linked </span>
-                    <FontAwesomeIcon className="mr-3" icon={faLinkedin} size="lg" />
+                    <FontAwesomeIcon className="mr-3 ac-icon" icon={faLinkedin} size="lg" />
                     <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" />
                     
                 </a>
                 <a className="ac-link mr-5" href="https://github.com/acosa188">
-                    <FontAwesomeIcon className="mr-3" icon={faGithub} size="lg" /> 
+                    <FontAwesomeIcon className="mr-3 ac-icon" icon={faGithub} size="lg" /> 
                     <span className="footer-font mr-3" > Github </span>
                     <FontAwesomeIcon icon={faExternalLinkAlt} size="xs" />
                 </a>
