eba876ab707504c9e143fc175baa80392f5bb3fb
Arjun Cosare <arjun.cosare@outlook.com>
2020-04-29 19:25:26 -0600

    Updates

---
+++ precache-manifest.45417759bc280c4b5922811497d7a00e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde8c4fecc4c77d9a42c2f25a5203e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25be78629a1e6518dea",
+    "url": "/static/css/main.19a61987.chunk.css"
+  },
+  {
+    "revision": "31e6a86fa32924a71ed5",
+    "url": "/static/js/2.81901eae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.81901eae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25be78629a1e6518dea",
+    "url": "/static/js/main.ce51e9be.chunk.js"
+  },
+  {
+    "revision": "c63f9a79ac3a7e1d9f34",
+    "url": "/static/js/runtime-main.b9d9df64.js"
+  },
+  {
+    "revision": "2758851a90307206915e4a0a584b9af1",
+    "url": "/static/media/IvaraTheMerchantPlan.2758851a.png"
+  },
+  {
+    "revision": "5af98ea35f04c24feb80d335a3789e54",
+    "url": "/static/media/canadaLive.5af98ea3.PNG"
+  },
+  {
+    "revision": "c96a9111df71b54ca093562371abecfd",
+    "url": "/static/media/canadaLivePlan.c96a9111.png"
+  },
+  {
+    "revision": "78546ec2fddf7343a904da070b83393e",
+    "url": "/static/media/dumpling.78546ec2.png"
+  },
+  {
+    "revision": "04de9916a69fc1a08e23c6367d4c6fd2",
+    "url": "/static/media/ivaraDiscord.04de9916.PNG"
+  }
+]);
